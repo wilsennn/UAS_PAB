3f407bdc28234325f89bf1fe776caa43b6c83479
wilsen <wilsenfendrius@mhs.mdp.ac.id>
2023-06-27 17:35:00 +0700

    final project pab

--- app/src/main/java/com/wilsen/petshop/API/RetroServer.java
+++ app/src/main/java/com/wilsen/petshop/API/RetroServer.java
@@ -4,7 +4,7 @@ import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
 public class RetroServer {
-    private static final String linkserver = "https://petshopwilsenfendrius.000webhostapp.com/";
+    private static final String linkserver = "https://petshopconnect.000webhostapp.com/";
     private static Retrofit retro;
 
     public static Retrofit konekRetrofit(){
